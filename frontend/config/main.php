56e5ddde9aa00296e21482b53e3bfd40478a69d1
tianqingshuai <tianqingshuai@baidu.com>
2016-04-13 00:38:56 +0800

    Add urlManager component

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -28,14 +28,11 @@ return [
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-            'rules' => [
-            ],
+            'rules' => require(dirname(__FILE__) . '/route.php'),
         ],
-        */
     ],
     'params' => $params,
 ];
---
+++ frontend/config/route.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * User: tianqingshuai
+ * Date: 2016/4/13
+ * url route
+ */
+return array(
+    '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+);
