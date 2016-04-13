27a52b58dd5897f2d9a075e6d67c8c6963cadc4f
tianqingshuai <tianqingshuai@baidu.com>
2016-04-14 01:36:02 +0800

    Add smarty3 extension

--- common/config/main.php
+++ common/config/main.php
@@ -1,9 +1,15 @@
 <?php
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+//    'defaultController' => 'main',
+
     'components' => [
         'cache' => [
             'class' => 'yii\caching\FileCache',
+//            'class' => YII_DEBUG ? 'system.caching.CFileCache' : 'system.caching.CApcCache',
         ],
+        'smarty' => array (
+            'class' => 'yii\smarty\smarty3',
+        ),
     ],
 ];
--- frontend/config/bootstrap.php
+++ frontend/config/bootstrap.php
@@ -1,1 +1,3 @@
 <?php
+Yii::setAlias('@app ', dirname(__DIR__));
+Yii::setAlias('@template', dirname(__DIR__) . '/template');
